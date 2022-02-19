602a464edcb9cce53b6a8e0605fe18472f6e77a2
mjpurin <mjpurin@gmail.com>
2022-02-19 10:18:34 +0900

    Http通信基礎

---
+++ Assets/HttpController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class HttpController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(HttpConnect());
+    }
+
+    IEnumerator HttpConnect() {
+        string url = "https://joytas.net/php/hello.php";
+        UnityWebRequest uwr = UnityWebRequest.Get(url);
+        yield return uwr.SendWebRequest();
+        if(uwr.isHttpError || uwr.isNetworkError) {
+            Debug.Log(uwr.error);
+        } else {
+            Debug.Log(uwr.downloadHandler.text);
+        }
+    }
+}
